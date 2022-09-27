fed601c9edfbf143004de05de8f385310fe92147
TDDDDDy <114346660+TDDDDDy@users.noreply.github.com>
2022-09-27 18:18:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '杨老师0',
+      name: '杨老师',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oytEm6tN728hl4qcE4QP2Yb_ipXI',
       // 你想对他发送的模板消息的模板ID
